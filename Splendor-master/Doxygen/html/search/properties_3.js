0f4c93427e4feafc6b4c6e5f15ab66db8eaea535
Zacharie Favre <zacharie.favre@cpnv.ch>
2018-11-16 13:50:09 +0100

    MaJ Doxygen

--- Splendor-master/Doxygen/html/search/properties_3.js
+++ Splendor-master/Doxygen/html/search/properties_3.js
@@ -1,4 +1,5 @@
 var searchData=
 [
-  ['name',['Name',['../class_splendor_1_1_player.html#a15abd489e523e11b6beb4b186783e47f',1,'Splendor::Player']]]
+  ['name',['Name',['../class_splendor_1_1_player.html#a15abd489e523e11b6beb4b186783e47f',1,'Splendor::Player']]],
+  ['nbprestige',['NbPrestige',['../class_splendor_1_1_player.html#ad92459d327fa7cc546259931af417aa1',1,'Splendor::Player']]]
 ];
